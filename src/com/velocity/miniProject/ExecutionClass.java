30d388e34665011cba2b23c0d68fe3e6914c47a4
ashwininangare <nangareashwini1997@gmail.com>
2023-01-10 14:07:10 +0530

    save changes

--- src/com/velocity/miniProject/ExecutionClass.java
+++ src/com/velocity/miniProject/ExecutionClass.java
@@ -2,6 +2,7 @@ package com.velocity.miniProject;
 import java.sql.SQLException;
 public class ExecutionClass {
 	public static void main(String[] args) throws SQLException {
+		
 		LogIn logIn = new LogIn();
 		logIn.mainPage();
 	}
