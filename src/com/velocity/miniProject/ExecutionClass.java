01a98d7830185f08fd97651f54a4d6f584cb6d60
ashwininangare <nangareashwini1997@gmail.com>
2023-01-10 13:52:01 +0530

    branch execution changes

--- src/com/velocity/miniProject/ExecutionClass.java
+++ src/com/velocity/miniProject/ExecutionClass.java
@@ -2,6 +2,8 @@ package com.velocity.miniProject;
 import java.sql.SQLException;
 public class ExecutionClass {
 	public static void main(String[] args) throws SQLException {
+		
+		System.out.println("this is execution class-branch changes");
 		LogIn logIn = new LogIn();
 		logIn.mainPage();
 	}
