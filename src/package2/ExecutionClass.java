fdaa1618d1e3a7c8b1328db519ae4156d8d733d4
AjinkyaBhosale <ajinkyabhosale009@gmail.com>
2023-01-11 14:39:36 +0530

    same class

--- src/package2/ExecutionClass.java
+++ src/package2/ExecutionClass.java
@@ -4,8 +4,7 @@ import java.sql.SQLException;
 import com.velocity.miniProject.LogIn;
 public class ExecutionClass {
 	public static void main(String[] args) throws SQLException {
-		System.out.println("this is test branch pakage2");
-		
+		System.out.println("conflicts in same class");
 		System.out.println("othr working in pakge1");
 		LogIn logIn = new LogIn();
 		logIn.mainPage();
