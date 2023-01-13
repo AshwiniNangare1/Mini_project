4a3ff4f2d526817b2193e6d372408a68917a0cce
AjinkyaBhosale <ajinkyabhosale009@gmail.com>
2023-01-13 14:14:25 +0530

    added

---
+++ src/package2/Demo.java
@@ -0,0 +1,11 @@
+package package2;
+
+public class Demo {
+public static void main(String[] args) {
+	System.out.println("Bug 11");
+	System.out.println("Bug 11");
+	System.out.println("Bug 11");
+	System.out.println("Bug 11");
+	
+}
+}
