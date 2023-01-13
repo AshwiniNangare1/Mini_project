31e2eea77d463c4c7552fef05d88cbac27c7111d
AjinkyaBhosale <ajinkyabhosale009@gmail.com>
2023-01-13 13:22:57 +0530

    Changes made in cart

--- src/package2/Cart.java
+++ src/package2/Cart.java
@@ -16,8 +16,8 @@ public class Cart {
 	PreparedStatement preparedStatement = null;
 	Scanner scanner = new Scanner(System.in);
 	public void addToCart(int customer_id,int product_id,int quantity) {
-		String customer_name ="";
-		String product_name = "";
+		String customer_name ="Ajinkya";
+		String product_name = "Gold Fish";
 		int product_price = 0;
 		Scanner scanner = new Scanner(System.in);
 		try {
